abc9c04a2d1b7196ba81f29ae386c515298f5c5c
Zakhar Pochuvalov <94977621+sobselena@users.noreply.github.com>
2026-03-05 23:41:11 +0300

    feat: add Authorization token to fetch requests

--- src/redux/api/usersAPI.ts
+++ src/redux/api/usersAPI.ts
@@ -7,9 +7,22 @@ export const usersApi = createApi({
   reducerPath: 'usersApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:4000/',
+    prepareHeaders: headers => {
+      const token = localStorage.getItem('accessToken');
+
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+
+      return headers;
+    },
   }),
   endpoints: builder => ({
-    getUserByLogin: builder.query({ query: (login: string) => `users/${login}` }),
+    getUserByLogin: builder.query({
+      query: (login: string) => ({
+        url: `users/${login}`,
+      }),
+    }),
 
     signUpUser: builder.mutation({
       query: (authData: SignUpRequest) => ({
