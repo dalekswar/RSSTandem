7587efb1188051188f2c5a27be9c3f2e3438c7c6
Zakhar Pochuvalov <94977621+sobselena@users.noreply.github.com>
2026-03-07 02:28:11 +0300

    refactor: remove TanStack Query provider from App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,12 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppToaster } from '../components/app-toaster';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1 * 1000,
-    },
-  },
-});
 
 export const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}></RouterProvider>
+    <>
+      <RouterProvider router={router} />
       <AppToaster />
-    </QueryClientProvider>
+    </>
   );
 };
